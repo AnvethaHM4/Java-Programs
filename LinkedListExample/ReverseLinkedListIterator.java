41a61401abf80778e2869a30f6a4d475fb2fe193
AnvethaHM4 <anuhm078@gmail.com>
2025-05-23 12:22:28 +0530

    Update ReverseLinkedListIterator.java

--- LinkedListExample/ReverseLinkedListIterator.java
+++ LinkedListExample/ReverseLinkedListIterator.java
@@ -3,7 +3,7 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.*;
-public class p7 {
+public class ReverseLinkedListIterator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
