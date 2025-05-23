cc8555fc21343dac8f2dc8e63cff9a0559690aab
AnvethaHM4 <anuhm078@gmail.com>
2025-05-23 12:23:05 +0530

    Update SwapLinkedListElements.java

--- LinkedListExample/SwapLinkedListElements.java
+++ LinkedListExample/SwapLinkedListElements.java
@@ -1,6 +1,6 @@
 //5.Write a Java program that swaps two elements ( first and third elements ) in a linked list ( using Collections.swap(l_list, 0, 2))
 import java.util.*;
-public class p10 {
+public class SwapLinkedListElements {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
