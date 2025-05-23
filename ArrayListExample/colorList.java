e0065c8f71ed4eb18ca2900b035ebaac054e5dbf
AnvethaHM4 <anuhm078@gmail.com>
2025-05-23 12:18:11 +0530

    Update colorList.java

--- ArrayListExample/colorList.java
+++ ArrayListExample/colorList.java
@@ -1,6 +1,6 @@
 //2.Write a java program for getting different colors through ArrayList interface and remove the 2nd element and color "Blue" from the ArrayList
 import java.util.*;
-public class p2 {
+public class colorList.java {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
