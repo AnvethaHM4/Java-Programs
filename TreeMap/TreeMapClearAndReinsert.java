46c031b1339fcf4cbd0b3789001d988b42b9bd4d
AnvethaHM4 <anuhm078@gmail.com>
2025-05-23 12:23:34 +0530

    Update TreeMapClearAndReinsert.java

--- TreeMap/TreeMapClearAndReinsert.java
+++ TreeMap/TreeMapClearAndReinsert.java
@@ -5,7 +5,7 @@ package lab2;
 import java.util.HashMap;
 import java.util.*;
 
-public class P6 {
+public class TreeMapClearAndReinsert {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
